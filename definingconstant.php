bb6f9b0e24bb1b8d2456f42b9411c812224d8319
Maria Joanne Alexandria M. Memorial <joannemmemorial@gmail.com>
2012-09-23 14:55:35 +0800

    PHP sample codes

---
+++ definingconstant.php
@@ -0,0 +1,6 @@
+<?php
+//define a constant
+define ('TODAY',' September 7,2012 ');
+
+echo 'Today is'  .TODAY.  'This servers as our official date for our session';
+?>
---
+++ variable.php
@@ -0,0 +1,7 @@
+<?php 
+//script created by Joanne Memorial
+//variable Declaration
+
+$tempf = 451;
+echo "Trees catch fire at ".(5/9) * ($tempf-32). "degrees";
+?>
